ca8091397035a49b81a266e2fbb2e88efd5c681d
RedEmber-hub <redember38@gmail.com>
2025-06-17 19:20:41 +0300

    добавила функцию переключения; добавила закрытие модального окна по пустому месту

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,16 +1,11 @@
 const modal = () => {
     const modal = document.querySelector('.popup')
     const buttons = document.querySelectorAll('.popup-btn');
-    const closeBtn = modal.querySelector('.popup-close');
 
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
             showModal();
         })
-    })
-
-    closeBtn.addEventListener('click', () => {
-        modal.style.display = 'none';
     })
 
     function showModal() {
@@ -31,6 +26,13 @@ const modal = () => {
 
         requestAnimationFrame(fadeIn);
     }
+
+    modal.addEventListener('click', (e) => {
+        if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
+            modal.style.display = 'none';
+        };
+
+    })
 
 }
 

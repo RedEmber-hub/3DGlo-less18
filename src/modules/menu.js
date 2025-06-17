aa860d41c1f5f1032e01536f70f7f4ffbf7c7b22
RedEmber-hub <redember38@gmail.com>
2025-06-17 19:31:56 +0300

    Используя делегирование событий сделать обработчики для:

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,21 +1,24 @@
 const menu = () => {
     const menuBtn = document.querySelector('.menu');
     const menu = document.querySelector('menu');
-    const closeBtn = menu.querySelector('.close-btn');
-    const menuItems = menu.querySelectorAll('ul>li>a');
 
     const handleMenu = () => {
         menu.classList.toggle('active-menu')
     }
 
-    menuBtn.addEventListener('click', () => {
-        handleMenu();
+    menuBtn.addEventListener('click', handleMenu);
+
+    menu.addEventListener('click', (e) => {
+        // закрыть по кнопке "крестик"
+        if (e.target.closest('.close-btn')) {
+            handleMenu();
+        }
+
+        // закрыть по пункту меню
+        if (e.target.closest('ul > li > a')) {
+            handleMenu();
+        }
     })
 
-    closeBtn.addEventListener('click', () => {
-        handleMenu();
-    })
-
-    menuItems.forEach(menuItem => menuItem.addEventListener('click', handleMenu))
 }
 export default menu;

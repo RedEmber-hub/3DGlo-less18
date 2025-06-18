9898aa170308e5abfad210aef4a414e26ae951da
RedEmber-hub <redember38@gmail.com>
2025-06-18 15:27:46 +0300

    клик произошел мимо меню, оно закрывается

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,15 +3,26 @@ const menu = () => {
     const menu = document.querySelector('menu');
 
     const toggleMenu = (e) => {
+        // клик по кнопке "меню"
         if (e.target === menuBtn || menuBtn.contains(e.target)) {
             menu.classList.toggle('active-menu');
+            return;
         }
 
+        // клик по пунктам в меню
         if (e.target.closest('ul > li > a')) {
-            menu.classList.remove('active-menu')
+            menu.classList.remove('active-menu');
+            return;
         }
 
+        // клик по крестику в меню
         if (e.target.closest('.close-btn')) {
+            menu.classList.remove('active-menu');
+            return;
+        }
+
+        // клик вне меню
+        if (!menu.contains(e.target)) {
             menu.classList.remove('active-menu')
         }
     }

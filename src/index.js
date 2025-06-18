e758d2360c66d255fc508a8f92bb9112efa1a9c7
RedEmber-hub <redember38@gmail.com>
2025-06-18 18:57:47 +0300

    реализовала слайдер по видео

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from './modules/menu';
 import modal from './modules/modal';
 import inputValidation from './modules/inputValidation';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 timer('2 july 2025');
 menu();
 modal();
 inputValidation();
 tabs();
+slider();

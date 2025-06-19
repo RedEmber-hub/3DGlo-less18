6e8b4ff735276470ca0dc8e2e6120c29505fb4da
RedEmber-hub <redember38@gmail.com>
2025-06-19 18:59:32 +0300

    реализовала калькулятор по видево

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal';
 import inputValidation from './modules/inputValidation';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
+import calc from './modules/calc'
 
 timer('2 july 2025');
 menu();
@@ -11,3 +12,4 @@ modal();
 inputValidation();
 tabs();
 slider();
+calc(100);
